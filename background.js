88cab2562e433475c04117c7942915cea685099c
Rupal Totale <rupaltotale@gmail.com>
2020-08-09 23:02:24 -0700

    id field added to note

--- background.js
+++ background.js
@@ -1,6 +1,18 @@
 chrome.runtime.onInstalled.addListener(function () {
   chrome.storage.sync.set(
-    { color: '#3aa757', notes: ['note 1', 'note 2'] },
+    {
+      notes: [
+        {
+          id: 1,
+          value: 'Dump any text in the text area above and hit enter to save',
+        },
+        {
+          id: 2,
+          value:
+            'View saved notes here, and copy or delete them with the click of a button! Try it out on this note :)',
+        },
+      ],
+    },
     function () {
       console.log('Setting storage');
     }
--- popup.js
+++ popup.js
@@ -1,33 +1,33 @@
-const notesDiv = document.getElementById("notes");
-const dumpArea = document.getElementById("dumpArea");
+const notesDiv = document.getElementById('notes');
+const dumpArea = document.getElementById('dumpArea');
 dumpArea.focus();
 const createNoteElement = (note) => {
-  const noteItem = document.createElement("div");
+  const noteItem = document.createElement('div');
   noteItem.innerHTML = note;
 
-  const buttonGroup = document.createElement("div");
-  buttonGroup.className = "d-flex flex-row";
-  buttonGroup.appendChild(createButton("Copy", () => copyNote(note)));
-  buttonGroup.appendChild(createButton("Delete", () => deleteNote(note)));
+  const buttonGroup = document.createElement('div');
+  buttonGroup.className = 'd-flex flex-row';
+  buttonGroup.appendChild(createButton('Copy', () => copyNote(note)));
+  buttonGroup.appendChild(createButton('Delete', () => deleteNote(note)));
 
-  const noteDiv = document.createElement("div");
+  const noteDiv = document.createElement('div');
   noteDiv.className =
-    "d-flex flex-row align-items-center alert alert-dark justify-content-between";
+    'd-flex flex-row align-items-center alert alert-dark justify-content-between';
   noteDiv.appendChild(noteItem);
   noteDiv.appendChild(buttonGroup);
   notesDiv.appendChild(noteDiv);
 };
-chrome.storage.sync.get("notes", ({ notes = [] }) => {
+chrome.storage.sync.get('notes', ({ notes = [] }) => {
   notes.forEach((note) => {
-    createNoteElement(note);
+    createNoteElement(note.value);
   });
 });
 
 const createButton = (name, onclickFunc) => {
-  const btn = document.createElement("button");
+  const btn = document.createElement('button');
   btn.innerHTML = name;
   btn.onclick = onclickFunc;
-  btn.className = "btn btn-light mx-2";
+  btn.className = 'btn btn-light mx-2';
   return btn;
 };
 
@@ -41,14 +41,19 @@ const deleteNote = (note) => {
 
 const saveNote = (e) => {
   const newNote = e.target.value;
-  if (!newNote.replace(/\s/g, "").length) {
+  if (!newNote.replace(/\s/g, '').length) {
     return;
   }
-  chrome.storage.sync.get("notes", ({ notes }) => {
-    const newNotes = notes.concat(newNote);
+  chrome.storage.sync.get('notes', ({ notes }) => {
+    const nextId = notes.slice(-1)[0].id + 1;
+    newNoteObject = {
+      id: nextId,
+      value: newNote,
+    };
+    const newNotes = notes.concat(newNoteObject);
     chrome.storage.sync.set({ notes: newNotes }, () => {
       createNoteElement(newNote);
-      dumpArea.value = "";
+      dumpArea.value = '';
     });
   });
 };
