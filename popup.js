16fb298c10931ca93be9fdd424134396e989d6f1
YashTotale <totaleyash@gmail.com>
2020-08-09 22:44:30 -0700

    copy/delete buttons added

--- popup.js
+++ popup.js
@@ -1,24 +1,53 @@
 const notesDiv = document.getElementById("notes");
 const dumpArea = document.getElementById("dumpArea");
 dumpArea.focus();
+const createNoteElement = (note) => {
+  const noteItem = document.createElement("div");
+  noteItem.innerHTML = note;
+
+  const buttonGroup = document.createElement("div");
+  buttonGroup.className = "d-flex flex-row";
+  buttonGroup.appendChild(createButton("Copy", () => copyNote(note)));
+  buttonGroup.appendChild(createButton("Delete", () => deleteNote(note)));
+
+  const noteDiv = document.createElement("div");
+  noteDiv.className =
+    "d-flex flex-row align-items-center alert alert-dark justify-content-between";
+  noteDiv.appendChild(noteItem);
+  noteDiv.appendChild(buttonGroup);
+  notesDiv.appendChild(noteDiv);
+};
 chrome.storage.sync.get("notes", ({ notes = [] }) => {
   notes.forEach((note) => {
-    const noteItem = document.createElement("p");
-    noteItem.innerHTML = note;
-    notesDiv.appendChild(noteItem);
+    createNoteElement(note);
   });
 });
+
+const createButton = (name, onclickFunc) => {
+  const btn = document.createElement("button");
+  btn.innerHTML = name;
+  btn.onclick = onclickFunc;
+  btn.className = "btn btn-light mx-2";
+  return btn;
+};
+
+const copyNote = (note) => {
+  console.log(`copying ${note}`);
+};
+
+const deleteNote = (note) => {
+  console.log(`deleting ${note}`);
+};
+
 const saveNote = (e) => {
   const newNote = e.target.value;
-  if (!newNote) {
+  if (!newNote.replace(/\s/g, "").length) {
     return;
   }
   chrome.storage.sync.get("notes", ({ notes }) => {
     const newNotes = notes.concat(newNote);
-    chrome.storage.sync.set({ newNotes }, () => {
-      const noteItem = document.createElement("p");
-      noteItem.innerHTML = newNote;
-      notesDiv.appendChild(noteItem);
+    chrome.storage.sync.set({ notes: newNotes }, () => {
+      createNoteElement(newNote);
       dumpArea.value = "";
     });
   });
